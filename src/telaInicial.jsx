cf137c36f23a52378728354e81f72b4365d36893
Ingrid3ds2025 <thamires.carvalho.carneiro@escola.pr.gov.br>
2025-06-12 09:18:04 -0300

    Add files via upload

--- src/telaInicial.jsx
+++ src/telaInicial.jsx
@@ -1,18 +1,15 @@
-import { Component } from "react";
-import {View, Text, Image, StyleSheet, TouchableOpacity} from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcon";
+import React,{ Component } from "react";
+import {View, Text, Image, StyleSheet,SafeAreaView, TouchableOpacity} from "react-native";
 
 class TelaInicial extends Component{
     render(){ 
         return( 
             <SafeAreaView style={styles.container}>
                 <View>
-                    <Text>
-
-                    </Text>
+                    <Text> Meu App </Text>
 
                     <TouchableOpacity>
-                        <Icon name={isOpenMenu ? 'close' : 'menu' } size={30} color="000"/>
+                        <Text style={styles.menu}> {'\u2630'}</Text>
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
@@ -26,10 +23,12 @@ export default TelaInicial;
 const styles= StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
         backgroundColor: 'black',
         marginBottom: 50,
     },
 
+    menu: {
+        color: 'white',
+        fontSize: 50,
+    }
 });
