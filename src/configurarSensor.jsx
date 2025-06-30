653c5d50d9114ad758cbba0a39ef5762628aab9f
Ingrid3ds2025 <thamires.carvalho.carneiro@escola.pr.gov.br>
2025-06-30 11:25:48 -0300

    Add files via upload

--- src/configurarSensor.jsx
+++ src/configurarSensor.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet,Text, TextInput} from 'react-native';
+import {View, StyleSheet, Text, TextInput, TouchableOpacity} from 'react-native';
 
 
 const ConfigurarSensor = () => {
@@ -9,21 +9,21 @@ const ConfigurarSensor = () => {
   return (
     <View style={styles.container}>
 
+      {/*TITULO */}
+      <Text style={styles.titulo}>VAMOS CONFIGURAR SEU SENSOR!</Text>
+
       {/*FORMULARIO PARA PREENCHIMENTO DOS DADOS */}
       <View style={styles.formulario}>
 
-        {/*TITULO */}
-        <Text style={styles.titulo}>VAMOS CONFIGURAR SEU SENSOR!</Text>
-
         {/*INSIRIR NOME DO SENSOR */}
-        <Text style={styles.subtitulo}>DE UM NOME PARA O SEU SENSOR</Text>
+        <Text style={styles.subtitulo}>DE UM NOME PARA O SEU SENSOR:</Text>
         <TextInput
           style={styles.input}
           onChangeText={onChangeText}
           value={text}
         />
 
-        {/*INSIRIR NOME DO SENSOR */}
+        {/*INSIRIR TAMANHO DA ÁREA */}
         <Text style={styles.subtitulo}> QUAL O TAMANHO DA ÁREA DE ONDE ELE FICA?</Text>
         <TextInput
           style={styles.input}
@@ -31,22 +31,43 @@ const ConfigurarSensor = () => {
           value={text}
         />
 
-        {/*INSIRIR NOME DO SENSOR */}
-        <Text style={styles.subtitulo}>DE UM NOME PARA O SEU SENSOR</Text>
+        {/*INSIRIR CULTURA */}
+        <Text style={styles.subtitulo}>QUAL CULTURA É CULTIVADA LÁ?</Text>
         <TextInput
           style={styles.input}
           onChangeText={onChangeText}
           value={text}
         />
 
-        {/*INSIRIR NOME DO SENSOR */}
-        <Text style={styles.subtitulo}>DE UM NOME PARA O SEU SENSOR</Text>
+        {/*INSIRIR ENDEREÇO */}
+        <Text style={styles.subtitulo}>QUAL É O ENDEREÇO DA SUA PROPRIEDADE?</Text>
         <TextInput
           style={styles.input}
           onChangeText={onChangeText}
           value={text}
         />
+
+        {/*INSIRIR DATA DE INSTALAÇÃO */}
+        <Text style={styles.subtitulo}>QUAL É A DATA DE INSTALAÇÃO DO SENSOR?</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+
+        {/*INSIRIR RESPONSÁVEL PELO PRODUTO*/}
+        <Text style={styles.subtitulo}>QUEM É O RESPONSÁVEL PELO SENSOR?</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+  
       </View>
+
+      <TouchableOpacity style={styles.botao} >
+            <Text style={styles.textBotao}>Configurar</Text>
+        </TouchableOpacity>
     </View>
   );
 };
@@ -62,23 +83,44 @@ const styles = StyleSheet.create({
     titulo: {
         color: 'white',
         fontSize:20,
-        fontWeight: '600',
-        marginTop: 20,
+        fontWeight: '700',
+        marginTop: 10,
        
       },
 
     formulario: {
-        color: 'white',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start'
     },
 
     input: {
         color: 'white',
         height: 40,
-        margin: 12,
+        width: '70',
         borderWidth: 1,
         borderColor: 'green',
         padding: 10,
+        marginTop: 15
     },
+
+    subtitulo: {
+      color: 'green',
+      marginTop: 15
+    },
+
+    botao:{
+      backgroundColor: 'green',
+      height: 40,
+      width: 300,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 20
+    },
+
+    textBotao:{
+      fontSize: 20,
+      fontWeight: '500'
+    }
 });
 
 export default ConfigurarSensor;
